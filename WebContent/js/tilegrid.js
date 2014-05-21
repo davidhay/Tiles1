9f67273337be20f95e197a9142cd941963ff1efa
David Hay <david.j.hay@gmail.com>
2014-05-21 09:39:11 +0100

    hole is an object. moving towards all tiles being objects.

--- WebContent/js/tilegrid.js
+++ WebContent/js/tilegrid.js
@@ -1,5 +1,9 @@
+var TileGridException = function(message) {
+   this.message = message;
+   this.name = "TileGridException";
+};
 var TileGrid = function(num_rows, num_cols, getInitial) {
-	console.log(num_rows, num_cols, getInitial);
+	console.log(num_rows, num_cols, getInitial);	
 	this.num_rows = num_rows;
 	this.num_cols = num_cols;
 	var that = this;
@@ -26,75 +30,87 @@ var TileGrid = function(num_rows, num_cols, getInitial) {
 	}// for loop
 
 
-	console.log('rows are ', this.rows);
-
 	// 0,0 is top,left of grid!
 	// 2,5 is the 2nd row, 5th column
 
-	this.hole_r = 2;
-	this.hole_c = 2;
+	this.hole = {
+			r:2,
+			c:2,
+			above : function(){
+				return {r:this.r-1, c:this.c };
+			},
+			below : function(){
+				return {r:this.r+1, c:this.c };
+			},
+			left : function(){
+				return {r:this.r, c:this.c-1 };
+			},
+			right : function(){
+				return {r:this.r, c:this.c+1 };
+			},
+			tile : function(){
+				return that.rows[this.r][this.c];
+			},
+			update : function(r,c){
+				this.r = r;
+				this.c = c;
+			}
+	};
+	
+	this.hole.tile().value ='';
 
-	var tile = this.rows[this.hole_r][this.hole_c];
-	tile.value = '';// the hole has no contents.
-
+	console.log('hole',this.hole);
+	console.log('hole : above',this.hole.above());
+	console.log('hole : below',this.hole.below());
+	console.log('hole : left',this.hole.left());
+	console.log('hole : right',this.hole.right());
+	
+	this.swap = function(coords){
+		console.log('swap with r=['+coords.r+']c=['+coords.c+']');		
+		var other = this.rows[coords.r][coords.c];
+		this.hole.tile().value = other.value;
+		other.value = null;
+		this.hole.update(coords.r,coords.c);		
+	};
+	this.moveHoleUp = function(){
+		 console.log('moveHoleUp');
+		 this.swap(this.hole.above());
+	};
+	this.moveHoleDown = function(){
+		console.log('moveHoleDown');
+		this.swap(this.hole.below());
+	};
+	this.moveHoleLeft = function(){
+		console.log('moveHoleLeft');
+		this.swap(this.hole.left());
+	};
+	this.moveHoleRight = function(){
+		console.log('moveHoleRight');
+		this.swap(this.hole.right());
+	};
+	this.reverse = function(move){
+		switch(move){
+			case this.moveHoleUp    : return this.moveHoleDown(); break;
+			case this.moveHoleDown  : return this.moveHoleUp(); break;
+			case this.moveHoleLeft  : return this.moveHoleRight(); break;
+			case this.moveHoleRight : return this.moveHoleLeft(); break;
+			default  : throw new TileGridException("problem trying to get reverse for "+move);
+		}
+	};
+	
 	this.selectCell = function(r, c) {
-		
-		console.log('select cell row=',r,'cell=',c);
-		
-		var row = that.rows[r];
-		var holeLeftOrRight = function(next_c) {
-			var contents_next = row[next_c].value;
-			row[that.hole_c].value = contents_next; // old hole
-			that.hole_c = next_c;
-			row[that.hole_c].value = null; // new hole		
-		};
-		var holeLeft = function() {
-			if (that.hole_c == 0) {
-				throw new Exception("cannot move hole left!");
-			}
-			var next_c = that.hole_c - 1;
-			holeLeftOrRight(next_c);
-		};
-		var holeRight = function() {			
-			if (that.hole_c + 1 == num_cols) {
-				throw new Exception("cannot move hole right!");
-			}
-			var next_c = that.hole_c + 1;
-			holeLeftOrRight(next_c);
-		};
-		var holeUpOrDown = function(next_r){
-			var contents_next = that.rows[next_r][c].value;
-			that.rows[that.hole_r][c].value = contents_next; // old hole
-			that.hole_r = next_r;
-			that.rows[that.hole_r][c].value = null; // new hole
-		};
-		var holeUp = function() {
-			if (that.hole_r == 0) {
-				throw new Exception("cannot move hole up!");
-			}
-			console.log('holeUp');
-			var next_r = that.hole_r - 1;
-			holeUpOrDown(next_r);
-		};
-		var holeDown = function() {
-			if (that.hole_r + 1 == that.num_rows) {
-				throw new Exception("cannot move hole up!");
-			}
-			console.log('holeDown');
-			var next_r = that.hole_r + 1;
-			holeUpOrDown(next_r);
-		};
+		console.log('selectCell',r,c);
 		var slideVertical = function() {
 			console.log('slideVertical');
 			do {
-				r < that.hole_r ? holeUp() : holeDown();
-			} while (that.hole_r != r);
+				r < that.hole.r ? that.moveHoleUp() : that.moveHoleDown();
+			} while (that.hole.r != r);
 		};
 		var slideHorizontal = function() {
 			console.log('slideHorizontal');
 			do {
-				c < that.hole_c ? holeLeft() : holeRight();
-			} while (that.hole_c != c );
+				c < that.hole.c ? that.moveHoleLeft() : that.moveHoleRight();
+			} while (that.hole.c != c );
 		};
 		if (r < 0 || r >= that.num_rows) {
 			return;
@@ -102,22 +118,18 @@ var TileGrid = function(num_rows, num_cols, getInitial) {
 		if (c < 0 || c >= that.num_cols) {
 			return;
 		}
-		if (c == that.hole_c && r == that.hole_r) {
+		if (c == that.hole.c && r == that.hole.r) {
 			return;
 		}
 
-		if (r == that.hole_r) {
+		if (r == that.hole.r) {
 			console.log('selected cell is on same row as hole');
 			slideHorizontal();
-		} else if (c == that.hole_c) {
+		} else if (c == that.hole.c) {
 			console.log('selected cell is on same column as hole');
 			slideVertical();
 		}
 
-	};
-
-	this.getContent = function(r, c) {
-		return grid[r][c];
 	};
 
 };
